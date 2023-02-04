b11b308f2bdcb3935b3fbb5547ff2206f451d991
zachey <12keysjams@gmail.com>
2023-02-04 14:57:38 +0300

    profile new

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const NoPage = () => {
+	const navigate = useNavigate();
+	const adm = JSON.parse(localStorage.getItem('profile'));
 	return (
 		<div
 			style={{ minHeight: '90vh', background: 'white' }}
@@ -8,14 +10,24 @@ const NoPage = () => {
 		>
 			<div>
 				{' '}
-				<h6>Page Not Found</h6>
-				<ul>
-					<li>This can be caused by typing a wrong url or link</li>
-					<li>Check your internet connection to the internet</li>
-					<li>
+				<h5>Page Not Found</h5>
+				<ul className="py-2">
+					<li className="mt-2">
+						This can be caused by typing a wrong url or link
+					</li>
+					<li className="mt-2">
+						Check your internet connection to the internet
+					</li>
+					<li className="mt-2">
 						Probably a server error from our side,we are working on it
 					</li>
 				</ul>
+				<button
+					className="btn btn-primary"
+					onClick={() => navigate(`/v2/${adm?.result?._id}`)}
+				>
+					Try Again
+				</button>
 			</div>
 		</div>
 	);
